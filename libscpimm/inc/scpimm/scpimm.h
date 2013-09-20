5c14dfea2accaf4be8014da17fe9a66d6adacd95
Andrey Nakin <andrey.nakin@gmail.com>
2013-09-20 14:16:38 +0400

    modified:   libscpimm/inc/scpimm/scpimm.h 	libscpimm/src/

--- libscpimm/inc/scpimm/scpimm.h
+++ libscpimm/inc/scpimm/scpimm.h
@@ -4,21 +4,41 @@
 #include <stdint.h>
 
 /*
+	Initialize multimeter
+*/
+void SCPIMM_setup();
+
+/*
 	Process value measured
 */
 void SCPIMM_acceptValue(double value);
 void SCPIMM_parseInBuffer(const char* buf, size_t len);
 
 /******************************************************************************
+  Basic constants
+******************************************************************************/
+
+#define SCPIMM_OVERFLOW 9.90000000E+37
+
+/******************************************************************************
   Multimeter mode constants (to use in MM_setMode)
 ******************************************************************************/
 
 #define SCPIMM_MODE_DCV	0
-#define SCPIMM_MODE_ACV	1
-#define SCPIMM_MODE_DCC	2
-#define SCPIMM_MODE_ACC	3
-#define SCPIMM_MODE_RESISTANCE_2W	4
-#define SCPIMM_MODE_RESISTANCE_4W	5
+#define SCPIMM_MODE_DCV_RATIO	1
+#define SCPIMM_MODE_ACV	2
+#define SCPIMM_MODE_DCC	3
+#define SCPIMM_MODE_ACC	4
+#define SCPIMM_MODE_RESISTANCE_2W	5
+#define SCPIMM_MODE_RESISTANCE_4W	6
+
+/******************************************************************************
+  Range constants
+******************************************************************************/
+
+#define SCPIMM_RANGE_MIN -1.0
+#define SCPIMM_RANGE_MAX -2.0
+#define SCPIMM_RANGE_DEF -3.0
 
 /******************************************************************************
   Following functions must be defined
@@ -67,6 +87,16 @@ void SCPIMM_triggerMeasurement();
   Send response to serial port
 */
 size_t SCPIMM_send(const uint8_t* buf, size_t len);
+
+/* 
+  Turn "remote control" mode to on/off
+*/
+void SCPIMM_remote(const bool remote);
+
+/* 
+  Issue a short (up to 500 ms) beep
+*/
+void SCPIMM_beep();
 
 #endif	//	__EXTERNALS_H_SCPIMM
 
