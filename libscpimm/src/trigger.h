5c590e349d5eae022cc97d169d491be070646df8
Andrey Nakin <andrey.nakin@gmail.com>
2015-04-14 19:08:15 +0300

    modified:   inc/scpimm/errors.h 	modified:   inc/scpimm/scpimm.h 	modified:   src/configure.c 	modified:   src/configure.h 	modified:   src/display.c 	modified:   src/display.h 	modified:   src/dmm.c 	modified:   src/dmm.h 	modified:   src/ieee488.c 	modified:   src/ieee488.h 	modified:   src/input.c 	modified:   src/input.h 	modified:   src/measure.c 	modified:   src/measure.h 	modified:   src/route.c 	modified:   src/route.h 	modified:   src/sample.c 	modified:   src/sample.h 	modified:   src/scpimm.c 	modified:   src/scpimm_internal.h 	modified:   src/sense.c 	modified:   src/sense.h 	modified:   src/system.c 	modified:   src/system.h 	modified:   src/trigger.c 	modified:   src/trigger.h 	modified:   src/utils.c 	modified:   src/utils.h 	modified:   test/default_multimeter.c 	modified:   test/default_multimeter.h 	modified:   test/test_configure.c 	modified:   test/test_display.c 	modified:   test/test_dmm.c 	modified:   test/test_generic.c 	modified:   test/test_ieee488.c 	modified:   test/test_input.c 	modified:   test/test_route.c 	modified:   test/test_sample.c 	modified:   test/test_sense.c 	modified:   test/test_system.c 	modified:   test/test_trigger.c 	modified:   test/test_utils.c 	modified:   test/test_utils.h

--- libscpimm/src/trigger.h
+++ libscpimm/src/trigger.h
@@ -1,3 +1,20 @@
+/*
+	This file is part of scpi-multimeter library.
+
+	v7-28-arduino is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	v7-28-arduino is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with v7-28-arduino.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __TRIGGER_H_SCPIMM_
 #define	__TRIGGER_H_SCPIMM_
 
